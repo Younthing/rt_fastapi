47429222843828f3cd262cd22e0843c4b2fae6c1
Younthing <fanxingfu3344@gmail.com>
2025-05-12 23:51:07 +0800

    doc: 降低使用负担

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -2,15 +2,15 @@
 
 docker build -t deepchem-api:cpu -f docker/dockerfile .
 
-## 运行（交互式）
+## 运行
 
-# 可以增加-p参数映射网络端口，-v 参数映射本地文件
-docker run --rm -it deepchem-api:cpu
-docker run -p 8000:8000 deepchem-api:cpu
+docker run  --rm -p 8000:8000 deepchem-api:cpu
 ## 保存
 
-
-
+docker save -o deepchem-api.tar deepchem-api:cpu
 ## 发送 
 
-## 加载 略
+scp deepchem-api.tar root@<your-server-ip>:/path/to/destination
+
+## 加载 
+docker load -i deepchem-api.tar
